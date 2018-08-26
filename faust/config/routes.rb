b78a30bfea26eab3b153562cc671559da44dcef7
Joanna Davidson <joanna@shrikesong.com>
2018-08-26 18:44:22 -0400

    Moved assets over. Created table for info, will redo in divs, pulled info over

--- faust/config/routes.rb
+++ faust/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'products/list'
-  get 'products/detail'
 
   root 'products#list'
-  
+
+  get 'products/:id' => 'products#detail'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
